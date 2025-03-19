e3e6c77e7654e27553f23e137df532b0586cd39e
Sergei Kotlyachkov <kotl@users.noreply.github.com>
2025-03-19 13:22:37 -0400

    Add bootstrap parameter support to skip terraform

--- idx-template.nix
+++ idx-template.nix
@@ -5,8 +5,13 @@
     chmod -R +w "$out"
 
     # Apply project ID to configs
-    sed -e 's/<project-id>/${projectId}/' ${.idx/dev.nix} > "$out/.idx/dev.nix"
-
+    if [ !-z $bootstrapjs ]
+    then
+      sed -e 's/<project-id>/${projectId}/' ${.idx/dev.nix} | sed -e 's/terraform init/# skip/' | sed -e 's/terraform apply/# skip/' > "$out/.idx/dev.nix"
+      echo '${bootstrapjs}' > '$out/src/bootstrap.js'
+    else
+      sed -e 's/<project-id>/${projectId}/' ${.idx/dev.nix} > "$out/.idx/dev.nix"
+    fi
     # Remove the template files themselves and any connection to the template's
     # Git repository
     rm -rf "$out/.git" "$out/idx-template".{nix,json} "$out/node_modules"
