6dd472051c65658fe1908d01410bc60edd00fda3
Sergei Kotlyachkov <kotl@users.noreply.github.com>
2025-03-19 15:07:42 -0400

    Final fix for boostrapJs parameter to work

--- idx-template.nix
+++ idx-template.nix
@@ -5,10 +5,10 @@
     chmod -R +w "$out"
     echo 'bootstrapJs was set to: ${bootstrapJs}'
     # Apply project ID to configs
-    if [ !-z '${bootstrapJs}' ]
+    if [ ! -z '${bootstrapJs}' ]
     then
       sed -e 's/<project-id>/${projectId}/' ${.idx/dev.nix} | sed -e 's/terraform init/# skip/' | sed -e 's/terraform apply/# skip/' > "$out/.idx/dev.nix"
-      echo '${bootstrapJs}' > '$out/src/bootstrap.js'
+      echo '${bootstrapJs}' > "$out/src/bootstrap.js"
     else
       sed -e 's/<project-id>/${projectId}/' ${.idx/dev.nix} > "$out/.idx/dev.nix"
     fi
